cfe34a114a4fa97f65a015b2747f5570f960cf95
Nicholas <romitinicholas@gmail.com>
2021-11-19 17:40:24 +0100

    prima versione funzionante

---
+++ script.js
@@ -0,0 +1,72 @@
+const arrayEmployees = [
+    {
+        nameUtente: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nameUtente: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        nameUtente: "Angela Lopez",
+        role: "Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nameUtente: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        nameUtente: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+for (i = 0; i < arrayEmployees.length; i++) {
+    createTeamCard();
+}
+
+// programma per iserire le cards
+let nameUtente = "";
+let role = "";
+let image = "";
+let file = "";
+
+const addMember = document.getElementById("addMemberButton").addEventListener("click", function () {
+    getData();
+    console.log(arrayEmployees);
+})
+
+function getData() {
+    nameUtente = document.getElementById("name").value;
+    role = document.getElementById("role").value;
+    image = document.getElementById("image").value;
+    file = {nameUtente,role,image};
+    arrayEmployees.push(file);
+    createCard(file); 
+    console.log(file);    
+    return file;  
+}
+
+function createCard(file) {
+    document.querySelector(".team-container").innerHTML += `<div class="team-card">
+            <div class="card-image">
+            <img src="img/${file.image}"/></div>
+            <div class="card-text">
+            <h3>${file.nameUtente}</h3>
+            <p>${file.role}</p></div></div>`;
+}
+
+function createTeamCard() {
+    document.querySelector(".team-container").innerHTML += `<div class="team-card">
+    <div class="card-image">
+    <img src="img/${arrayEmployees[i].image}"/></div>
+    <div class="card-text">
+    <h3>${arrayEmployees[i].nameUtente}</h3>
+    <p>${arrayEmployees[i].role}</p></div></div>`;
+}
+
